4268c5e65ff6aa83185ba901cc2e52c7ce1f1317
Victor Denissen <vdenisse@student.s19.be>
2024-01-08 13:24:55 +0100

    fixed src/tokenizer/libft/src/memory/ft_free_array.c include to correctly point to libft.h

--- src/tokenizer/libft/src/memory/ft_free_array.c
+++ src/tokenizer/libft/src/memory/ft_free_array.c
@@ -6,11 +6,11 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 11:27:42 by wdevries          #+#    #+#             */
-/*   Updated: 2023/11/06 14:14:29 by wdevries         ###   ########.fr       */
+/*   Updated: 2024/01/08 13:24:02 by vdenisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../inc/libft.h"
 
 void	ft_free_array(char **array, size_t size)
 {
