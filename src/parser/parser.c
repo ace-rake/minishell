3c63427c1a4dbc916d8473f2df0af1d83949bbf3
Warre Devriese <wdevries@a2-r2-p3.s19.be>
2024-01-10 13:44:21 +0100

    Full parser without ANY testing. Write a main to compile next

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,92 +6,37 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 08:29:23 by wdevries          #+#    #+#             */
-/*   Updated: 2024/01/10 11:01:47 by wdevries         ###   ########.fr       */
+/*   Updated: 2024/01/10 13:38:58 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-t_token	*parse_pipes(t_token **tokens)
+int	token_is_redirection(t_token *token)
 {
-	t_token *head;
-	t_token	*prev_pipe;
-	int		i;
-
-	head = NULL;
-	prev_pipe = NULL;
-	i = 0;
-	while (tokens[i++]);
-	while (i--)
-	{
-		if (tokens[i]->type == PIPE)
-		{
-			if (head == NULL)
-				head = tokens[i];
-			else
-			{
-				tokens[i]->parent = prev_pipe;
-				prev_pipe->left = tokens[i];
-			}
-			prev_pipe = tokens[i];
-		}
-	}
-	return (head);
-}
-
-t_token	*parse_redirections(t_token **tokens, t_parser_redir_utils *u, int i)
-{
-	u->head = NULL;
-	u->prev_redir = NULL;
-	u->pipe = NULL;
-	while (i-- && tokens[i]->type != PIPE)
-	{
-		if (tokens[i]->type == REDIR_IN || tokens[i]->type == REDIR_OUT
-			|| tokens[i]->type == REDIR_HEREDOC || tokens[i]->type == REDIR_APPEND)
-		{
-			if (u->prev_redir)
-			{
-				tokens[i]->right = u->prev_redir;
-				u->prev_redir->parent = tokens[i];
-			}
-			u->prev_redir = tokens[i];
-			u->head = tokens[i];
-		}
-	}
-	return (i);
-}
-
-t_token	*parse_redirections(t_token **tokens)
-{
-	t_parser_redir_utils	u;
-	int	i;
-
-	i = 0;
-	while (tokens[i++]);
-	while (i > 0)
-	{
-		i = parse_redirections(tokens, &u, i);
-		u.pipe = tokens[i];
-		u.pipe->right = u.head;
-		u.head->parent = u.pipe;
-	}
-	if (u.pipe)
-	{
-		u.pipe->left = u.head;
-		u.head->parent = u.pipe;
-		return NULL
-	}
-	else 
-		return (head);
-}
-
-t_token	*parse_commands(t_token **tokens)
-{
-
+	return (token->type == REDIR_IN || token->type == REDIR_OUT
+			|| token->type == REDIR_HEREDOC || token->type == REDIR_APPEND);
 }
 
 t_token	*parser(t_token **tokens)
 {
-	t_token	*ast_head;
+	t_token *ast_head;
+	t_token	*ret1;
+	t_token	*ret2;
+	t_token	*ret3;
+	t_token	*ret4;
 
+	ret1 = parse_pipes(tokens);
+	ret2 = parse_redirections(tokens);
+	ret3 = parse_commands(tokens);
+	ret4 = parse_arguments(tokens);
+	if (ret1)
+		ast_head = ret1;
+	else if (ret2)
+		ast_head = ret2;
+	else if (ret3);
+		ast_head = ret3;
+	else if (ret4)
+		ast_head = ret4;
+	return (ast_head);
 }
--- src/parser/pipes.c
+++ src/parser/pipes.c
@@ -1,27 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.h                                           :+:      :+:    :+:   */
+/*   pipes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 08:27:25 by wdevries          #+#    #+#             */
-/*   Updated: 2024/01/10 13:41:32 by wdevries         ###   ########.fr       */
+/*   Created: 2024/01/10 13:34:37 by wdevries          #+#    #+#             */
+/*   Updated: 2024/01/10 13:36:41 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_H
-# define PARSER_H
+t_token	*parse_pipes(t_token **tokens)
+{
+	t_token *head;
+	t_token	*prev_pipe;
+	int		i;
 
-# include "../../typedefs.h"
-
-
-t_token	*parser(t_token **tokens);
-t_token	*parse_pipes(t_token **tokens);
-t_token	*parse_pipes(t_token **tokens);
-t_token	*parse_commands(t_token **tokens);
-t_token	*parse_arguments(t_token **tokens);
-//util
-int	token_is_redirection(t_token *token);
-
-# endif
+	head = NULL;
+	prev_pipe = NULL;
+	i = 0;
+	while (tokens[i++]);
+	while (i--)
+	{
+		if (tokens[i]->type == PIPE)
+		{
+			if (head == NULL)
+				head = tokens[i];
+			else
+			{
+				tokens[i]->parent = prev_pipe;
+				prev_pipe->left = tokens[i];
+			}
+			prev_pipe = tokens[i];
+		}
+	}
+	return (head);
+}
