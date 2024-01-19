636c7a3a8bbcdd98bd2694871c126ebfe55a9e9a
Warre Devriese <wdevries@a2-r4-p3.s19.be>
2024-01-19 12:02:07 +0100

    Warre's part of main

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
-int	main(void)
-{
-	return (0);
+#include "typedefs.h"
+#include "tokenizer.h"
+#include "lexer.h"
+#include "parser.h"
+#include "executor.h"
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s 'command'\n", argv[0]);
+        return 1;
+    }
+
+    char *input = argv[1];
+    t_token **tokens;
+    t_token *ast_head;
+
+    tokens = tokenizer(input);
+    lexer(tokens);
+    ast_head = parser(tokens);
+    return 0;
 }
+
