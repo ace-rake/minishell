10ac7ead6335e1cfe8af671e3b2f7242715f9e51
Victor Denissen <vdenisse@student.s19.be>
2024-01-19 12:07:06 +0100

    read_input.c

---
+++ src/prep/read_input.c
@@ -0,0 +1,31 @@
+#include "env_parser.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	read_heredoc(t_token *token)
+{
+	char *str;
+	int	filedes[2];
+
+	if (pipe(filedes) == -1)
+		return (1);
+	token->input = filedes[0];
+	str = readline(">");
+	while (ft_strcmp(str, (const char *)token->right->value) != 0)
+	{
+		ft_putendl_fd(str, filedes[1]);
+		str = readline(">");
+	}
+	ft_putchar_fd('\0', filedes[1]);
+	return (0);
+}
+
+
+/*
+ * read line
+ * check if VALUE
+ * if not value
+ * 		write \n for prev line 
+ * 		and write own line
+ */
