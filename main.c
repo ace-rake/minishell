ebf7945ed6e95a56ba7974a9f1d71edb2dfa9dcf
Victor Denissen <vdenisse@student.s19.be>
2024-01-19 12:05:08 +0100

    victors part of main

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 #include "lexer.h"
 #include "parser.h"
 #include "executor.h"
+#include "env_parser.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char *envs) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s 'command'\n", argv[0]);
         return 1;
@@ -17,6 +18,9 @@ int main(int argc, char **argv) {
     tokens = tokenizer(input);
     lexer(tokens);
     ast_head = parser(tokens);
+
+	t_env_list *env = env_parser(envs);
+	execute_token(ast_head, env);	
     return 0;
 }
 
