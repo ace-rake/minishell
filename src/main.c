9c641cd9c286049fb970f21df853051bda9fac25
Warre Devriese <wdevries@a2-r4-p3.s19.be>
2024-01-22 15:32:16 +0100

    Added ctrl-C in interactive mode

--- src/main.c
+++ src/main.c
@@ -1,34 +1,12 @@
 #include "../inc/minishell.h"
-
-int	input_main(int argc, char *argv[], char *envs[])
-{
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s 'command'\n", argv[0]);
-        return 1;
-    }
-
-    char *input = argv[1];
-    t_token **tokens;
-    t_token *ast_head;
-
-    tokens = tokenizer(input);
-	if (!lexer(tokens))
-	{
-		//TODO: free tokens
-		return (1);
-	}
-    ast_head = parser(tokens);
-
-	t_env_list *env = env_parser(envs);
-	exec_token(ast_head, env);	
-	free_env(env);
-    return (0);
-
-}
-
 #include <readline/readline.h>
 #include <readline/history.h>
 
+void	sigint_handler(int signum)
+{
+	(void)signum;
+	write(STDOUT_FILENO, "\nminishell: ", 12);
+}
 
 int	loop_main(char *envs[])
 {
@@ -36,7 +14,6 @@ int	loop_main(char *envs[])
     t_token *ast_head;
 	t_env_list *env;
 	char *input;
-
 	while (1)
 	{
 		input = readline("minishell: ");
@@ -60,6 +37,13 @@ int	loop_main(char *envs[])
 }
 
 int main(int argc, char **argv, char *envs[]) {
+	struct sigaction sa;
+
+	sa.sa_handler = sigint_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+
 	return (loop_main(envs));
 	(void)argc;
 	(void)argv;
