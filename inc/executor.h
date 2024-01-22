0802877c6e286d136c6e5cabec19651fbfb86cc2
Victor Denissen <vdenisse@student.s19.be>
2024-01-22 15:35:13 +0100

    header update

--- inc/executor.h
+++ inc/executor.h
@@ -6,7 +6,7 @@
 /*   By: vdenisse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 14:30:16 by vdenisse          #+#    #+#             */
-/*   Updated: 2024/01/22 14:11:38 by vdenisse         ###   ########.fr       */
+/*   Updated: 2024/01/22 15:32:19 by vdenisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,20 @@ char **token_chain_to_array(t_token *token);
 
 //this token is the cmd token
 int	exec_command_file(t_token *token, t_env_list *env);
+int check_child(int *child_status);
+int	executor(t_token *token, t_env_list *env);
 
 char *get_full_cmd_path(char *cmd, t_env_list *env);
 
-int	exec_token(t_token *token, t_env_list *env);
+void	exec_token(t_token *token, t_env_list *env);
 
-int	env_builtin(t_token *token, t_env_list *env);
-int	cd_builtin(t_token *token, t_env_list *env);
-int	pwd_builtin(t_token *token);
-int		echo_builtin(t_token *token);
+void	env_builtin(t_token *token, t_env_list *env);
+void	cd_builtin(t_token *token, t_env_list *env);
+void	pwd_builtin(t_token *token);
+void	echo_builtin(t_token *token);
 void	exit_builtin(t_token *token, t_env_list *env);
-int	unset_builtin(t_token *token, t_env_list *env);
-int	export_builtin(t_token *token, t_env_list *env);
+void	unset_builtin(t_token *token, t_env_list *env);
+void	export_builtin(t_token *token, t_env_list *env);
 
 char *get_env_val(t_env_list *env, char *var);
 #endif
