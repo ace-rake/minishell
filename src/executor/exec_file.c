9e8983f1587216277654eccfb86c5f02194f1f95
Victor Denissen <vdenisse@student.s19.be>
2024-01-24 12:12:04 +0100

    revert closing of fds in cmd execute

--- src/executor/exec_file.c
+++ src/executor/exec_file.c
@@ -6,7 +6,7 @@
 /*   By: vdenisse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:23:49 by vdenisse          #+#    #+#             */
-/*   Updated: 2024/01/24 11:32:43 by vdenisse         ###   ########.fr       */
+/*   Updated: 2024/01/24 12:11:06 by vdenisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,8 +70,8 @@ int	exec_command_file(t_token *token, t_env_list *env)
 	{
 		int status;
 		waitpid(child, &status, 0);
-		close(token->input);
-		close(token->output);
+//		close(token->input);
+//		close(token->output);
 		g_in_command = 0;
 		free(cmd_path);
 		free(args);
