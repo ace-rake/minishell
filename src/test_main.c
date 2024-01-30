f58e1979538fb8fda70c609964ff39d2dbf9bca6
Victor Denissen <vdenisse@student.s19.be>
2024-01-30 18:58:46 +0100

    made test main to test all the command in commands.txt, you can run make test

---
+++ src/test_main.c
@@ -0,0 +1,61 @@
+#include "../inc/minishell.h"
+
+volatile sig_atomic_t g_in_command = 0;
+
+void	sigint_handler(int signum)
+{
+	(void)signum;
+	write(STDOUT_FILENO, "\n", 1);
+	if (!g_in_command)
+	{
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+int	cmd_main(char *envs[])
+{
+    t_token **tokens;
+    t_token *ast_head;
+	t_env_list *env;
+	char input[1024];
+	int	exit_status;
+	int monitor;
+	FILE *file = fopen("commands.txt", "r" );
+
+	exit_status = 0;
+	env = env_parser(envs);
+	while (1)
+	{
+		tokens = NULL;
+		ast_head = NULL;
+		fscanf(file, "%[^\n]\n", input);
+		printf("\ntesting line [%s]\n", input);
+		add_history(input);
+		monitor = tokenizer(input, &tokens);
+		if (monitor)
+			monitor = lexer(tokens);
+		if (monitor)
+			monitor = parser(tokens, &ast_head);
+		if (monitor)
+			monitor = expander(tokens, env, exit_status);
+		if (monitor)	
+			exit_status = executor(ast_head, env);
+		/* printf("retval main : [%i]\n",retval); */	
+		free_tokens(tokens);
+	}
+	free_env(env);
+
+	return (0);
+}
+
+int main(int argc, char **argv, char *envs[]) {
+	/* rl_catch_signals = 0; */
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+	cmd_main(envs);
+	(void)argc;
+	(void)argv;
+	(void)envs;
+}
