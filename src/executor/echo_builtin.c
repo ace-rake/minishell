2a3ae1174bc97e42ab3bbcadd4abb852d86b7b38
Victor Denissen <vdenisse@student.s19.be>
2024-02-01 10:36:08 +0100

    echo can now handle stuf like -nnnnnnn or -n -n -n - n

--- src/executor/echo_builtin.c
+++ src/executor/echo_builtin.c
@@ -6,57 +6,77 @@
 /*   By: vdenisse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 13:16:24 by vdenisse          #+#    #+#             */
-/*   Updated: 2024/01/30 11:48:56 by vdenisse         ###   ########.fr       */
+/*   Updated: 2024/02/01 10:35:36 by vdenisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-static int	echo_check_option(t_token **token, bool *option)
+static bool	check_single_token(t_token *token)
 {
-	int	index;
+	int	i;
 
-	if (!(*token)->right)
-		return (0);
-	index = 0;
-	if ((*token)->right->value[0] == '-')
+	if (!token || !token->value)
+		return (false);
+	if (token->value[0] != '-')
+		return (false);
+	if (!token->value[1])
+		return (false);
+	i = 0;
+	while (token->value[++i] && token->value[i] == 'n')
+		;
+	if (!token->value[i])
+		return (true);
+	return (false);
+}
+
+static int	echo_check_option_v2(t_token **token, bool *option)
+{
+	bool	cont;
+	t_token	*tmp;
+
+	cont = true;
+	tmp = *token;
+	while (cont)
 	{
-		while ((*token)->right->value[++index] != '\0')
-		{
-			if ((*token)->right->value[index] != 'n')
-				*option = false;
-			else
-				*option = true;
-		}
+		tmp = tmp->right;
+		cont = check_single_token(tmp);
+		if (!(*option) && cont)
+			*option = true;
 	}
-	if (*option)
-		*token = (*token)->right;
+	*token = tmp;
 	return (0);
 }
+
+// loop over every token
+// stop once a non -n or -nnnn combo is found
+// advance
 
 int	echo_builtin(t_token *token)
 {
 	bool	option;
 	char	**token_chain;
 	int		iter;
+	t_token	*echo;
 
+	echo = token;
 	option = false;
-	echo_check_option(&token, &option);
-	if (token->right)
+	echo_check_option_v2(&token, &option);
+	if (token)
 	{
 		token_chain = NULL;
-		token_chain = token_chain_to_array(token->right);
+		token_chain = token_chain_to_array(token);
 		iter = 0;
 		while (token_chain && token_chain[iter])
 		{
-			ft_putstr_fd(token_chain[iter], token->output);
+			ft_putstr_fd(token_chain[iter], echo->output);
 			if (token_chain[++iter])
-				ft_putchar_fd(' ', token->output);
+				ft_putchar_fd(' ', echo->output);
 		}
 		free(token_chain);
 	}
 	if (!option)
-		write(token->output, "\n", 1);
+		write(echo->output, "\n", 1);
 	return (0);
 }
 //TODO token_chain_to_array can fail
