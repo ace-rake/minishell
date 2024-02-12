50656c884098d29ec6e04933e39e2a6411307ef4
Warre Devriese <wdevries@a2-r4-p3.s19.be>
2024-02-12 12:54:42 +0100

    Fixed the case of multiple redirections before cmd

--- src/parser/commands.c
+++ src/parser/commands.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:09:40 by wdevries          #+#    #+#             */
-/*   Updated: 2024/02/02 11:33:40 by wdevries         ###   ########.fr       */
+/*   Updated: 2024/02/12 12:53:43 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,26 @@ static void	handle_pipe(t_token **tokens, t_parser_utils_commands *u)
 	}
 }
 
+static void	get_redirection(t_token **tokens, t_parser_utils_commands *u)
+{
+	int	i;
+
+	i = u->i;
+	while (i && tokens[i]->type != PIPE)
+	{
+		if (token_is_redirection(tokens[i]))
+		   u->redirection = tokens[i];
+		i--;
+	}
+}
+
 static void	handle_command(t_token **tokens, t_parser_utils_commands *u)
 {
 	u->command = tokens[u->i];
 	if (u->i >= 2 && tokens[u->i - 1]->type == ARGUMENT
 		&& token_is_redirection(tokens[u->i - 2]))
 	{
-		u->redirection = tokens[u->i - 2];
+		get_redirection(tokens, u);
 		u->redirection->left = u->command;
 		u->command->parent = u->redirection;
 	}
